eaa59fa146b1ad2ccc1d88123aa1bcade6269bdc
Helios-rabiller-LP <helios.rabiller@laplateforme.io>
2025-01-23 15:36:01 +0100

    feat : main menu made to be able to click play and quit

--- main_menu.py
+++ main_menu.py
@@ -1,2 +1,34 @@
-def menu():
-    print ("menu")
+def main_menu():
+    menu_running = True
+    while menu_running:
+        screen.fill(white)
+
+        # Title
+        title_surface = font.render("Hangman Game", True, black)
+        title_rect = title_surface.get_rect(center=(width // 2, height // 4))
+        screen.blit(title_surface, title_rect)
+
+        # Options
+        play_surface = font.render("Play", True, black)
+        play_rect = play_surface.get_rect(center=(width // 2, height // 2))
+        screen.blit(play_surface, play_rect)
+
+        quit_surface = font.render("Quit", True, black)
+        quit_rect = quit_surface.get_rect(center=(width // 2, height // 2 + 100))
+        screen.blit(quit_surface, quit_rect)
+
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu_running = False
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if play_rect.collidepoint(event.pos):
+                    main_game()
+                elif quit_rect.collidepoint(event.pos):
+                    menu_running = False
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.flip()
